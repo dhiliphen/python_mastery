ceb89040a0c7cc6613718082c6cf1c6b2d90f1ca
dhiliphen <dhiliphen@gmail.com>
2026-07-07 11:47:24 +0530

    Implement count_even function
    Added function to count even numbers from arguments.

--- args.py
+++ args.py
@@ -36,4 +36,15 @@ def average(*args):
     
 print(average(4,5,6))
 
-
+# return the number of even numbers in arguments list - here it is 3
+def count_even(*args):
+    number_of_items = 0
+    
+    for _ in args:
+         if ( _ % 2 == 0):
+            number_of_items += 1
+         
+         
+    return number_of_items
+    
+print(count_even(4,8,6))
